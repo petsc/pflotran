32544b833f0b196be6cd6cb86dbc51be23350f67
Unknown <bphilip@cayenne1.lanl.gov>
2007-05-30 15:09:55 -0600

    committing changes that allow SAMR code to call pflowgrid_setup_index

---
+++ src/pims/samr/src/setup_hierarchy_info.F90
@@ -0,0 +1,54 @@
+subroutine f_setup_hierarchy_data(pgrid)
+
+ use pflow_gridtype_module
+ use pflow_grid_module
+
+ implicit none
+ 
+#include "include/finclude/petsc.h"
+
+ external pflow_setup_index
+ type(pflowGrid), pointer :: pgrid
+
+  interface
+     integer function hierarchy_number_levels(p_hierarchy)
+     PetscFortranAddr, intent(inout) :: p_hierarchy
+   end function hierarchy_number_levels
+
+   integer function level_number_patches(p_hierarchy, ln)
+     PetscFortranAddr, intent(inout) :: p_hierarchy
+     integer, intent(in) :: ln
+   end function level_number_patches
+
+   logical function is_local_patch(p_hierarchy, ln, pn)
+     PetscFortranAddr, intent(inout) :: p_hierarchy
+     integer, intent(in) :: ln
+     integer, intent(in) :: pn
+   end function is_local_patch
+
+   PetscFortranAddr function hierarchy_get_patch(p_hierarchy, ln, pn)
+     PetscFortranAddr, intent(inout) :: p_hierarchy
+     integer, intent(in) :: ln
+     integer, intent(in) :: pn
+   end function hierarchy_get_patch
+   
+  end interface
+ 
+  integer :: nlevels
+  integer :: npatches
+  integer :: ln
+  integer :: pn
+  logical :: islocal
+ 
+  nlevels =  hierarchy_number_levels(pgrid%p_samr_hierarchy)
+ 
+  do ln=0,nlevels-1
+     npatches = level_number_patches(pgrid%p_samr_hierarchy, ln )
+     do pn=0,npatches-1
+        if(associated(pgrid%patchlevel_info(ln+1)%patches(pn+1)%patch_ptr) .eq. .TRUE. ) then
+           call pflow_setup_index(pgrid, pgrid%patchlevel_info(ln+1)%patches(pn+1)%patch_ptr)
+        endif
+     end do
+  end do
+
+end subroutine f_setup_hierarchy_data
