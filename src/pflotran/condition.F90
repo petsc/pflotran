83fd461c5eceb0ceee54e6b8fb0dc5c72a5df0ea
Glenn Hammond <unknown>
2007-06-15 13:34:42 -0700

    Fixed of ndof instead of 2

--- src/pflotran/condition.F90
+++ src/pflotran/condition.F90
@@ -284,10 +284,11 @@ subroutine UpdateBoundaryConditions(grid)
     icond = grid%ibconn(iconnbc)
     itype = condition_array(icond)%ptr%itype
 
-    grid%xxbc(2,iconnbc) = tref
+    grid%xxbc(grid%ndof,iconnbc) = tref
     grid%iphasebc(iconnbc) = 1
     if (itype == 1) then
       grid%xxbc(1,iconnbc) = condition_array(icond)%ptr%cur_value
+      grid%xxbc(grid%ndof,iconnbc) = condition_array(icond)%ptr%cur_value
     elseif (itype == 3 .or. itype == 4) then ! correct for pressure gradient
       datum_coord = 0.d0
       datum_coord(1) = condition_array(icond)%ptr%xdatum
@@ -310,6 +311,7 @@ subroutine UpdateBoundaryConditions(grid)
       value = pref - delp
 #endif
       grid%xxbc(1,iconnbc) = value
+      grid%xxbc(grid%ndof,iconnbc) = tref
     elseif (itype == 5) then
       grid%iphasebc(iconnbc) = 3
 !      icap = icap_p(grid%nL2G(grid%mblkbc(iconnbc)))
@@ -415,6 +417,7 @@ subroutine ComputeInitialCondition(grid, icondition)
     endif     
 
     xx_p(1+(iln-1)*grid%ndof) = value  
+    xx_p(grid%ndof+(iln-1)*grid%ndof) = tref  
     
   enddo
               
