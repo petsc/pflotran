961692ee79b33a77dfade3b63f0bd41acaef4ff2
Glenn Hammond <unknown>
2007-08-30 15:54:54 -0700

    merge

--- src/pflotran/hdf5_output.F90
+++ src/pflotran/hdf5_output.F90
@@ -23,6 +23,7 @@ module hdf5_output_module
 #define PHASE 9
 
   integer :: hdferr
+  logical, save :: first = .true.
   PetscErrorCode :: ierr
 
   public :: OutputHDF5
@@ -36,6 +37,7 @@ subroutine OutputHDF5(grid)
   type(pflowGrid) :: grid
 
   integer(HID_T) :: file_id
+  integer(HID_T) :: grp_id
   integer(HID_T) :: file_space_id
   integer(HID_T) :: data_set_id
   integer(HID_T) :: prop_id
@@ -47,7 +49,7 @@ subroutine OutputHDF5(grid)
   PetscScalar, pointer :: v_ptr
   
   character(len=32) :: filename = "pflow.h5"
-  character(len=32) :: string, string2
+  character(len=128) :: string
   real*8, pointer :: array(:)
   integer :: i
 
@@ -59,31 +61,49 @@ subroutine OutputHDF5(grid)
 #ifndef SERIAL_HDF5
   call h5pset_fapl_mpio_f(prop_id,PETSC_COMM_WORLD,MPI_INFO_NULL,hdferr);
 #endif
-  call h5fcreate_f(filename,H5F_ACC_TRUNC_F,file_id,hdferr,H5P_DEFAULT_F,prop_id)
+  if (.not.first) call h5fopen_f(filename,H5F_ACC_RDWR_F,file_id,hdferr,prop_id)
+  if (hdferr < 0 .or. first) then 
+    call h5fcreate_f(filename,H5F_ACC_TRUNC_F,file_id,hdferr,H5P_DEFAULT_F,prop_id)
+  endif
   call h5pclose_f(prop_id,hdferr)
   
-  ! write out coordinates in x, y, and z directions
-  string = "X-coordinates"
-  allocate(array(grid%nx))
-  array = grid%x(1:grid%nx)
-  call WriteCoordinate(string,grid,grid%nx,array,file_id)
-  deallocate(array)
+  if (first) then
 
-  string = "Y-coordinates"
-  allocate(array(grid%ny))
-  do i=1,grid%ny
-    array(i) = grid%y(i*grid%nx)
-  enddo
-  call WriteCoordinate(string,grid,grid%ny,array,file_id)
-  deallocate(array)
+    ! create a group for the coordinates data set
+    string = "Coordinates"
+    call h5gcreate_f(file_id,string,grp_id,hdferr,OBJECT_NAMELEN_DEFAULT_F)
+  
+    ! write out coordinates in x, y, and z directions
+    string = "X-coordinates"
+    allocate(array(grid%nx))
+    array = grid%x(1:grid%nx)
+    call WriteCoordinate(string,grid,grid%nx,array,grp_id)
+    deallocate(array)
+  
+    string = "Y-coordinates"
+    allocate(array(grid%ny))
+    do i=1,grid%ny
+      array(i) = grid%y(i*grid%nx)
+    enddo
+    call WriteCoordinate(string,grid,grid%ny,array,grp_id)
+    deallocate(array)
+  
+    string = "Z-coordinates"
+    allocate(array(grid%nz))
+    do i=1,grid%nz
+      array(i) = grid%z(i*grid%nx*grid%ny)
+    enddo
+    call WriteCoordinate(string,grid,grid%nz,array,grp_id)
+    deallocate(array)
+    
+    call h5gclose_f(grp_id,hdferr)
+    
+  endif
 
-  string = "Z-coordinates"
-  allocate(array(grid%nz))
-  do i=1,grid%nz
-    array(i) = grid%z(i*grid%nx*grid%ny)
-  enddo
-  call WriteCoordinate(string,grid,grid%nz,array,file_id)
-  deallocate(array)
+  ! create a group for the data set
+  write(string,'('' Time('',i4,''):'',pg12.4,x,a1)') &
+        grid%flowsteps,grid%t/grid%tconv,grid%tunit
+  call h5gcreate_f(file_id,string,grp_id,hdferr,OBJECT_NAMELEN_DEFAULT_F)
   
   ! write out data sets  
   call DACreateGlobalVector(grid%da_1_dof,global,ierr)
@@ -94,60 +114,59 @@ subroutine OutputHDF5(grid)
   !call DAGlobalToNaturalBegin(grid%da_1_dof,global,INSERT_VALUES,natural,ierr)
   !call DAGlobalToNaturalEnd(grid%da_1_dof,global,INSERT_VALUES,natural,ierr)
   string = "Temperature"
-  call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_DOUBLE)
+  call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_DOUBLE)
 
   ! pressure
   call GetVarFromArray(grid,global,PRESSURE,0)
   string = "Pressure"
-  call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_DOUBLE)
+  call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_DOUBLE)
 
   ! liquid saturation
   call GetVarFromArray(grid,global,LIQUID_SATURATION,0)
   string = "Liquid Saturation"
-  call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_DOUBLE)  
+  call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_DOUBLE)  
 
   ! gas saturation
   call GetVarFromArray(grid,global,GAS_SATURATION,0)
   string = "Gas Saturation"
-  call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_DOUBLE) 
+  call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_DOUBLE) 
   
   ! liquid energy
   call GetVarFromArray(grid,global,LIQUID_ENERGY,0)
   string = "Liquid Energy"
-  call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_DOUBLE) 
+  call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_DOUBLE) 
   
   ! gas energy
   call GetVarFromArray(grid,global,GAS_ENERGY,0)
   string = "Gas Energy"
-  call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_DOUBLE) 
+  call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_DOUBLE) 
   
   ! mole fractions
   do i=1,grid%nspec
     call GetVarFromArray(grid,global,MOLE_FRACTION,i-1)
-    write(string2,'(i4)') i
-    string = "Mole Fraction(" // trim(adjustl(string2)) // ")"
-    call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_DOUBLE) 
+    write(string,'(''Mole Fraction('',i4,'')'')') i
+    call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_DOUBLE) 
   enddo
   
   ! Volume Fraction
   if (grid%rk > 0.d0) then
     call GetVarFromArray(grid,global,VOLUME_FRACTION,0)
     string = "Volume Fraction"
-    call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_DOUBLE) 
+    call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_DOUBLE) 
   endif
   
   ! phase
   call GetVarFromArray(grid,global,PHASE,0)
   string = "Phase"
-  call WriteDataSetFromVec(string,grid,global,file_id,H5T_NATIVE_INTEGER) 
+  call WriteDataSetFromVec(string,grid,global,grp_id,H5T_NATIVE_INTEGER) 
   
   ! call VecDestroy(natural,ierr)
   call VecDestroy(global,ierr)
 
+  call h5gclose_f(grp_id,hdferr)
   call h5fclose_f(file_id,hdferr)
   call h5close_f(hdferr)
-
-  stop
+  first = .false.
 
 end subroutine OutputHDF5
 
@@ -228,7 +247,8 @@ subroutine WriteDataSet(name,grid,array,file_id,data_type)
   integer :: rank
   
   integer, pointer :: int_array(:)
-  integer :: i
+  real*8, pointer :: double_array(:)
+  integer :: i, j, k, count, id
   integer(HSIZE_T) :: start(3), length(3), stride(3)
 
   ! memory space which is a 1D vector  
@@ -239,9 +259,17 @@ subroutine WriteDataSet(name,grid,array,file_id,data_type)
 
   ! file space which is a 3D block
   rank = 3
+#define INVERT
+#ifndef INVERT
   dims(1) = grid%nx
   dims(2) = grid%ny
   dims(3) = grid%nz
+#else
+! have to trick hdf5 for now with inverted ordering
+  dims(3) = grid%nx
+  dims(2) = grid%ny
+  dims(1) = grid%nz
+#endif
   call h5screate_simple_f(rank,dims,file_space_id,hdferr,dims);
 
 
@@ -251,12 +279,21 @@ subroutine WriteDataSet(name,grid,array,file_id,data_type)
   call h5pclose_f(prop_id,hdferr)
   
   ! create the hyperslab
+#ifndef INVERT
   start(1) = grid%nxs
   start(2) = grid%nys
   start(3) = grid%nzs
   length(1) =  grid%nlx
   length(2) =  grid%nly
   length(3) =  grid%nlz
+#else
+  start(3) = grid%nxs
+  start(2) = grid%nys
+  start(1) = grid%nzs
+  length(3) =  grid%nlx
+  length(2) =  grid%nly
+  length(1) =  grid%nlz
+#endif
   stride = 1
   call h5sselect_hyperslab_f(file_space_id,H5S_SELECT_SET_F,start,length, &
                              hdferr,stride,stride)
@@ -268,15 +305,45 @@ subroutine WriteDataSet(name,grid,array,file_id,data_type)
 #endif
   if (data_type == H5T_NATIVE_INTEGER) then
     allocate(int_array(grid%nlmax))
+#ifdef INVERT
+    count = 0
+    do k=1,grid%nlz
+      do j=1,grid%nly
+        do i=1,grid%nlx
+          id = k+(j-1)*grid%nlz+(i-1)*grid%nlyz
+          count = count+1
+          int_array(id) = int(array(count))
+        enddo
+      enddo
+    enddo
+#else
     do i=1,grid%nlmax
       int_array(i) = int(array(i))
     enddo
+#endif
     call h5dwrite_f(data_set_id,data_type,int_array,dims, &
                     hdferr,memory_space_id,file_space_id,prop_id)
     deallocate(int_array)
   else
+#ifdef INVERT
+    allocate(double_array(grid%nlmax))
+    count = 0
+    do k=1,grid%nlz
+      do j=1,grid%nly
+        do i=1,grid%nlx
+          id = k+(j-1)*grid%nlz+(i-1)*grid%nlyz
+          count = count+1
+          double_array(id) = int(array(count))
+        enddo
+      enddo
+    enddo
+    call h5dwrite_f(data_set_id,data_type,double_array,dims, &
+                    hdferr,memory_space_id,file_space_id,prop_id)  
+    deallocate(double_array)
+#else
     call h5dwrite_f(data_set_id,data_type,array,dims, &
                     hdferr,memory_space_id,file_space_id,prop_id)  
+#endif
   endif
   call h5pclose_f(prop_id,hdferr)
   call h5dclose_f(data_set_id,hdferr)
