7243607bc1c2f2e9689ec7a73118c2ac161f533e
Glenn Hammond <unknown>
2007-09-14 16:56:15 -0700

    Added more flexible and parallel bcs to hanford 300 scenario

--- src/mdt/Hanford300.h
+++ src/mdt/Hanford300.h
@@ -17,15 +17,23 @@ public:
   Hanford300(Grid **grid);
   virtual ~Hanford300();
 
-  void computeRiverBoundary(Grid *grid);
-  void computeWestBoundary(Grid *grid);
-  void computeSouthBoundary(Grid *grid);
-  void computeRechargeBoundary(Grid *grid);
+  void computeTopBoundary(Grid *grid, int complete);
+  void computeBottomBoundary(Grid *grid, int complete);
+  void computeNorthBoundary(Grid *grid, int complete);
+  void computeSouthBoundary(Grid *grid, int complete);
+  void computeEastBoundary(Grid *grid, int complete);
+  void computeWestBoundary(Grid *grid, int complete);
+  void flagGridCells(Grid *grid);
 
 private:
 
   Polygon *river_polygon;
   AsciiGrid **ascii_grids;
+
+  void setMaterialIdBasedOnNaturalId(int natural_id, int material_id,
+                                     Grid *grid);
+  void setActiveBasedOnNaturalId(int natural_id, int active,
+                                 Grid *grid);
 
 };
 
