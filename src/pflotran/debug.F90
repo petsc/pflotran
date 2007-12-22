8848a2d3b2bbad008fe2cc4aa7beb6abf3386170
Glenn Hammond <unknown>
2007-12-22 09:47:10 -0800

    Added debug src file to repository and updated makefile

---
+++ src/pflotran/debug.F90
@@ -0,0 +1,126 @@
+module Debug_module
+
+  implicit none
+  
+  private
+  
+  type, public :: pflow_debug_type
+    logical :: vecview_residual
+    logical :: vecview_solution
+    logical :: matview_Jacobian
+
+    logical :: print_numerical_derivatives
+  end type pflow_debug_type
+  
+  type, public :: ptran_debug_type
+    logical :: placeholder
+  end type ptran_debug_type
+
+  interface DebugRead
+    module procedure DebugReadPflow
+  end interface DebugRead
+  
+  
+  public :: DebugCreatePflow, DebugRead
+  
+contains
+
+! ************************************************************************** !
+!
+! DebugCreatePflow: Create object that stores debugging options for PFLOW
+! author: Glenn Hammond
+! date: 12/21/07
+!
+! ************************************************************************** !
+function DebugCreatePflow()
+
+  implicit none
+  
+  type(pflow_debug_type), pointer :: DebugCreatePflow
+  
+  type(pflow_debug_type), pointer :: debug
+  
+  allocate(debug)
+  
+  debug%vecview_residual = .false.
+  debug%vecview_solution = .false.
+  debug%matview_Jacobian = .false.
+  
+  debug%print_numerical_derivatives = .false.
+  
+  DebugCreatePflow => debug
+
+end function DebugCreatePflow
+
+! ************************************************************************** !
+!
+! DebugCreatePtran: Create object that stores debugging options for PFLOW
+! author: Glenn Hammond
+! date: 12/21/07
+!
+! ************************************************************************** !
+function DebugCreatePtran()
+
+  implicit none
+  
+  type(ptran_debug_type), pointer :: DebugCreatePtran
+  
+  type(ptran_debug_type), pointer :: debug
+  
+  allocate(debug)
+  
+  DebugCreatePtran => debug
+
+end function DebugCreatePtran
+
+! ************************************************************************** !
+!
+! DebugReadPflow: Reads debugging data from the input file
+! author: Glenn Hammond
+! date: 12/21/07
+!
+! ************************************************************************** !
+subroutine DebugReadPflow(debug,fid)
+
+  use Fileio_module
+  
+  implicit none
+
+#include "definitions.h"
+    
+  type(pflow_debug_type) :: debug
+  integer :: fid
+  
+  character(len=MAXSTRINGLENGTH) :: string, error_string
+  character(len=MAXWORDLENGTH) :: keyword, word, word2
+  integer :: ierr
+
+  ierr = 0
+  do
+  
+    call fiReadFlotranString(fid,string,ierr)
+
+    if (string(1:1) == '.' .or. string(1:1) == '/' .or. &
+        fiStringCompare(string,'END',3)) exit  
+
+    call fiReadWord(string,keyword,.true.,ierr)
+    call fiErrorMsg('keyword','DEBUG', ierr)   
+      
+    select case(trim(keyword))
+    
+      case('PRINT_SOLUTION','VECVIEW_SOLUTION','VIEW_SOLUTION')
+        debug%vecview_solution = .true.
+      case('PRINT_RESIDUAL','VECVIEW_RESIDUAL','VIEW_RESIDUAL')
+        debug%vecview_solution = .true.
+      case('PRINT_JACOBIAN','MATVIEW_JACOBIAN','VIEW_JACOBIAN')
+        debug%vecview_solution = .true.
+      case('PRINT_NUMERICAL_DERIVATIVES','VIEW_NUMERICAL_DERIVATIVES')
+        debug%print_numerical_derivatives = .true.
+
+    end select 
+  
+  enddo  
+
+end subroutine DebugReadPflow
+
+end module Debug_module
