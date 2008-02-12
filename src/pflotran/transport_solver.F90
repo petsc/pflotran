91583459763b13fea554a4818580477d1bea876b
Glenn Hammond <unknown>
2008-02-12 13:05:48 -0800

    Removed comments

--- src/pflotran/transport_solver.F90
+++ src/pflotran/transport_solver.F90
@@ -6,16 +6,9 @@ module Transport_Solver_module
  
 #include "definitions.h"
 
-! Apparently the PETSc authors believe that Fortran 90 modules should ensure
-! that PETSC_AVOID_DECLARATIONS and PETSC_AVOID_MPIF_H are defined when the
-! PETSc header files are included.  I can get around this, though, by making
-! the definitions in these headers private.
 #include "petscreldefs.h"
 #include "include/finclude/petscvec.h"
 #include "include/finclude/petscvec.h90"
-  ! It is VERY IMPORTANT to make sure that the above .h90 file gets included.
-  ! Otherwise some very strange things will happen and PETSc will give no
-  ! indication of what the problem is.
 #include "include/finclude/petscmat.h"
 #include "include/finclude/petscmat.h90"
 #include "include/finclude/petscksp.h"
