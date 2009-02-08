774a455ca4bbdbe8abeefe613eeed68bdc74ee8a
Unknown <lichtner@lanl.gov>
2009-02-08 10:26:36 -0700

    Added temperature dependence of activity coefficient parameters---somewhat of a hack!

--- src/pflotran/database.F90
+++ src/pflotran/database.F90
@@ -110,6 +110,44 @@ subroutine DatabaseRead(reaction,option)
     endif
   enddo
 
+  if (option%reference_temperature == 0.d0) then
+    reaction%debyeA = 0.4939d0 
+    reaction%debyeB = 0.3253d0 
+    reaction%debyeBdot = 0.0374d0
+  else if (option%reference_temperature == 25.d0) then
+    reaction%debyeA = 0.5114d0 
+    reaction%debyeB = 0.3288d0 
+    reaction%debyeBdot = 0.0410d0
+  else if (option%reference_temperature == 60.d0) then
+    reaction%debyeA = 0.5465d0 
+    reaction%debyeB = 0.3346d0 
+    reaction%debyeBdot = 0.0440d0
+  else if (option%reference_temperature == 100.d0) then
+    reaction%debyeA = 0.5995d0 
+    reaction%debyeB = 0.3421d0 
+    reaction%debyeBdot = 0.0460d0
+  else if (option%reference_temperature == 150.d0) then
+    reaction%debyeA = 0.6855d0 
+    reaction%debyeB = 0.3525d0 
+    reaction%debyeBdot = 0.0470d0
+  else if (option%reference_temperature == 200.d0) then
+    reaction%debyeA = 0.7994d0 
+    reaction%debyeB = 0.3639d0 
+    reaction%debyeBdot = 0.0470d0
+  else if (option%reference_temperature == 250.d0) then
+    reaction%debyeA = 0.9593d0 
+    reaction%debyeB = 0.3766d0 
+    reaction%debyeBdot = 0.0340d0
+  else if (option%reference_temperature == 300.d0) then
+    reaction%debyeA = 1.2180d0 
+    reaction%debyeB = 0.3925d0 
+    reaction%debyeBdot = 0.0000d0
+  else if (option%reference_temperature == 350.d0) then
+    reaction%debyeA = 1.2180d0 
+    reaction%debyeB = 0.3925d0 
+    reaction%debyeBdot = 0.0000d0
+  endif
+
   num_nulls = 0
   null_name = 'null'
   do ! loop over every entry in the database
