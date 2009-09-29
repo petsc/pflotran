b64db8e3c68d49abd4a72cff17d19101b2b604ac
Glenn Hammond <unknown>
2009-09-28 23:09:40 -0700

    Added Rubin1 scenario for ifrc site

---
+++ src/mdt/Rubin1.h
@@ -0,0 +1,43 @@
+#ifndef Rubin1_H_
+#define Rubin1_H_
+
+#include <string.h>
+
+#include "petsc.h"
+
+#include "Globals.h"
+#include "AsciiGrid.h"
+#include "BoundarySet.h"
+#include "Connection.h"
+#include "Grid.h"
+#include "Polygon.h"
+
+class Rubin1 {
+  
+public:
+  Rubin1(Grid **grid);
+  virtual ~Rubin1();
+
+  void computeTopBoundary(Grid *grid, PetscInt complete);
+  void computeBottomBoundary(Grid *grid, PetscInt complete);
+  void computeNorthBoundary(Grid *grid, PetscInt complete);
+  void computeSouthBoundary(Grid *grid, PetscInt complete);
+  void computeEastBoundary(Grid *grid, PetscInt complete);
+  void computeWestBoundary(Grid *grid, PetscInt complete);
+  void computeIFCBoundary(Grid *grid, Polygon *p);
+  void flagGridCells(Grid *grid);
+
+private:
+
+  Polygon *ifc_polygon;
+  Polygon *boundary_polygon;
+  AsciiGrid **ascii_grids;
+
+  void setMaterialIdBasedOnNaturalId(PetscInt natural_id, PetscInt material_id,
+                                     Grid *grid);
+  void setActiveBasedOnNaturalId(PetscInt natural_id, PetscInt active,
+                                 Grid *grid);
+
+};
+
+#endif /*Rubin1_H_*/
