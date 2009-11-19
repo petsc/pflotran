97805fd35bf00859beaf4d5f4c6e335e1026c641
Peter C. Lichtner <unknown>
2009-11-19 14:18:54 -0700

    Added copyright info.

--- src/pflotran/pflotran.F90
+++ src/pflotran/pflotran.F90
@@ -1,35 +1,41 @@
 !=======================================================================
-! PFLOTRAN v1.0 LA-CC-06-093
+! PFLOTRAN v2.0 LA-CC-09-047
 !=======================================================================
 
-! The software titled "PFLOTRAN v 1.0" has been assigned LA-CC-06-093. 
-! The software is unclassified and does not contain Unclassified 
-! Controlled Nuclear Information (UCNI).
-
-! The software titled "PFLOTRAN v 2.0" has been assigned LA-CC-09-047. 
-! The software is unclassified and does not contain Unclassified 
-! Controlled Nuclear Information (UCNI).
-
-! The software is under review to be released as open source, which 
-! requires review and approval by the appropriate DOE Program Office. 
-! If DOE declines to release this software as publicly available open 
-! source, it would be subject to export control under Department of 
-! Commerce regulations, and classified as ECCN 
-! (Export Control Classification Number) EAR99. 
- 
-! If released as open source, the software will be publicly available and 
-! not subject to export control. Until DOE approval is obtained, the 
-! software should be treated as export controlled. DOE reserves the 
-! right to release or deny release of the software as open source.
+!Copyright 2009. Los Alamos National Security, LLC. This material was produced under U.S. 
+!Government contract DE-AC52-06NA25396 for Los Alamos National Laboratory (LANL), which is operated 
+!by Los Alamos National Security, LLC for the U.S. Department of Energy. The U.S. Government has 
+!rights to use, reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS 
+!NATIONAL SECURITY, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE 
+!USE OF THIS SOFTWARE.  If software is modified to produce derivative works, such modified software 
+!should be clearly marked, so as not to confuse it with the version available from LANL.
+!Additionally, this library is free software; you can redistribute it and/or modify it under the 
+!terms of the GNU Lesser General Public License as published by the Free Software Foundation; 
+!either version 2.1 of the License, or (at your option) any later version. Accordingly, this 
+!library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even 
+!the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser 
+!General Public License for more details.
 
 ! Send all bug reports/questions/comments to:
+!
 ! Peter C. Lichtner
 ! Los Alamos National Laboratory
 ! Earth and Environmental Sciences
-! EES-6, MS: D469
+! EES-16, MS: D469
 ! (505) 667-3420
 ! lichtner@lanl.gov
 ! Los Alamos, NM
+
+! or
+
+! Glenn E. Hammond
+! Pacific Northwest National Laboratory
+! Energy and Environment Directorate
+! MSIN K9-36
+! (509) 395-3895
+! glenn.hammond@pnl.gov
+! Richland, WA
+
 !=======================================================================
   program pflotran
   
