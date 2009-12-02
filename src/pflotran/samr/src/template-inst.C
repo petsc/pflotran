ee35b462d8fff756e5a23a6840ecf61b1c3ba917
Unknown <philipb@chandra.local>
2009-12-02 01:32:27 -0500

    local

--- src/pflotran/samr/src/template-inst.C
+++ src/pflotran/samr/src/template-inst.C
@@ -13,10 +13,6 @@
 #include "CoarsenSchedule.C"
 #include "BoundaryConditionStrategy.h"
 #include "PflotranJacobianLevelOperator.h"
-#ifndef __PGI
-#include "VisInterlacedDataStrategy.h"
-#include "VisInterlacedDataStrategy.C"
-#endif
 
 template class SAMRAI::tbox::Pointer< SAMRAI::solv::SAMRAIVectorReal<NDIM,double> >;
 template class  SAMRAI::tbox::Array< SAMRAI::tbox::Pointer< SAMRAI::solv::SAMRAIVectorReal<NDIM,double> > >;
@@ -24,6 +20,3 @@ template class SAMRAI::tbox::Array<SAMRAI::PflotranJacobianLevelOperator*>;
 template class SAMRAI::tbox::Pointer<BoundaryConditionStrategy>;
 template class SAMRAI::tbox::Array< SAMRAI::tbox::Pointer< SAMRAI::xfer::CoarsenSchedule<NDIM> > >;
 template class SAMRAI::tbox::Array< SAMRAI::tbox::Array< SAMRAI::tbox::Pointer< SAMRAI::xfer::CoarsenSchedule<NDIM> > > >;
-#ifndef __PGI
-template void std::vector<SAMRAI::appu::VisInterlacedDataStrategy<NDIM>*, std::allocator<SAMRAI::appu::VisInterlacedDataStrategy<NDIM>*> >::_M_insert_aux(__gnu_cxx::__normal_iterator<SAMRAI::appu::VisInterlacedDataStrategy<NDIM>**, std::vector<SAMRAI::appu::VisInterlacedDataStrategy<NDIM>*, std::allocator<SAMRAI::appu::VisInterlacedDataStrategy<NDIM>*> > >, SAMRAI::appu::VisInterlacedDataStrategy<NDIM>* const&);
-#endif
