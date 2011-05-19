10217efa0af5017eabb509846ba80120ac50d674
Peter C. Lichtner <unknown>
2011-05-19 15:23:25 -0600

    Modified printout of warning message when log K = 500.

--- src/pflotran/reaction.F90
+++ src/pflotran/reaction.F90
@@ -4952,10 +4952,9 @@ subroutine ReactionFitLogKCoef(coefs,logK,name,option,reaction)
       if (dabs(logK(i) - 500.) < 1.d-10) then
         iflag = 1
         temp_int(i) = ZERO_INTEGER
-      else if (logK(i) .gt. 500.) then
         option%io_buffer = 'In ReactionFitLogKCoef: log K .gt. 500 for ' // &
-                           trim(name) // '---stop!'
-        call printErrMsg(option)
+                           trim(name)
+        call printWrnMsg(option)
       else
         coefs(j) = coefs(j) + vec(j,i)*logK(i)
         temp_int(i) = ONE_INTEGER
