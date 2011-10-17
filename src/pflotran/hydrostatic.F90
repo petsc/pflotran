67e773ba015f2b2b487abb95aa8ac3a3a0d1eb5c
Glenn Hammond <unknown>
2011-10-17 09:46:56 -0700

    Added conditional to skip temp/conc in hydrostatic if not dirichlet

--- src/pflotran/hydrostatic.F90
+++ src/pflotran/hydrostatic.F90
@@ -81,11 +81,13 @@ subroutine HydrostaticUpdateCoupler(coupler,option,grid)
       piezometric_head_gradient(1:3) = condition%general%liquid_pressure%gradient%cur_value(1:3)
     case default
       ! for now, just set it; in future need to account for a different temperature datum
-      if (associated(condition%temperature)) then
+      if (associated(condition%temperature) .and.
+          condition%temperature%itype /= DIRICHLET_BC) then
         temperature_at_datum = condition%temperature%dataset%cur_value(1)
         temperature_gradient(1:3) = condition%temperature%gradient%cur_value(1:3)
       endif
-      if (associated(condition%concentration)) then
+      if (associated(condition%concentration) .and.
+          condition%temperature%itype /= DIRICHLET_BC) then
         concentration_at_datum = condition%concentration%dataset%cur_value(1)
         concentration_gradient(1:3) = condition%concentration%gradient%cur_value(1:3)
       endif
