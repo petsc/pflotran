39aa9c3a9dee903f266cdcb006ded7d5a2f62bb0
Glenn Hammond <unknown>
2012-03-16 14:46:05 -0700

    Moved error statement regarding use of isotherm rxns with aqueous complexation to database.F90, and placed within a conditional

--- src/pflotran/database.F90
+++ src/pflotran/database.F90
@@ -2783,6 +2783,12 @@ subroutine BasisInit(reaction,option)
   ! Kd reactions
   
   if (reaction%neqkdrxn > 0) then
+
+    if (reaction%neqcplx > 0) then
+      option%io_buffer = 'Isotherm reactions currently calculated as a ' // &
+                         'function of free-ion, not totals.  Contact Glenn!'
+      call printErrMsg(option)
+    endif
   
     ! allocate arrays
     allocate(reaction%eqkdspecid(reaction%neqkdrxn))
