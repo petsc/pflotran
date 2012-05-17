2265b425f4093cc0a62094dbb8ae0bd84d4055fd
Gautam Bisht <gb9@ornl.gov>
2012-05-17 11:11:33 -0400

    Corrected area computation in UCellComputeArea() subroutine

--- src/pflotran/unstructured_cell.F90
+++ src/pflotran/unstructured_cell.F90
@@ -254,7 +254,7 @@ function UCellComputeArea(cell_type,vertices,option)
       v2(2) = vertices(1)%y-vertices(4)%y
       v2(3) = vertices(1)%z-vertices(4)%z
       n2 = CrossProduct(v1,v2)
-      area1 = 0.5d0*sqrt(DotProduct(n2,n2))
+      area2 = 0.5d0*sqrt(DotProduct(n2,n2))
       
       UCellComputeArea = (area1 + area2)
     case(TRI_TYPE)
