688c8abac7666d9c0e737bac58a4439bfb3fef46
Glenn Hammond <unknown>
2012-08-31 10:35:21 -0700

    Added printing of processor decomposition to pflotran.out file

--- src/pflotran/init.F90
+++ src/pflotran/init.F90
@@ -269,18 +269,25 @@ subroutine Init(simulation)
 !  endif  
   
 
-  if (OptionPrintToScreen(option)) then
-    ! general print statements for both flow and transport modes
-    write(*,'(/,"++++++++++++++++++++++++++++++++++++++++++++++++++++&
-      &++++++++")')
-    if ((realization%discretization%itype == STRUCTURED_GRID).or.&
-        (realization%discretization%itype == STRUCTURED_GRID_MIMETIC)) then
-      write(*,'(" Requested processors and decomposition = ",i5,", npx,y,z= ",3i5)') &
-        option%mycommsize,grid%structured_grid%npx,grid%structured_grid%npy, &
-        grid%structured_grid%npz
-      write(*,'(" Actual decomposition: npx,y,z= ",3i5)') &
-        grid%structured_grid%npx_final,grid%structured_grid%npy_final, &
-        grid%structured_grid%npz_final
+  if (realization%discretization%itype == STRUCTURED_GRID .or. &
+      realization%discretization%itype == STRUCTURED_GRID_MIMETIC) then
+    if (OptionPrintToScreen(option)) then
+      write(*,'(/," Requested processors and decomposition = ", &
+               & i5,", npx,y,z= ",3i4)') &
+          option%mycommsize,grid%structured_grid%npx, &
+          grid%structured_grid%npy,grid%structured_grid%npz
+      write(*,'(" Actual decomposition: npx,y,z= ",3i4,/)') &
+          grid%structured_grid%npx_final,grid%structured_grid%npy_final, &
+          grid%structured_grid%npz_final
+    endif
+    if (OptionPrintToScreen(option)) then
+      write(option%fid_out,'(/," Requested processors and decomposition = ", &
+                           & i5,", npx,y,z= ",3i4)') &
+          option%mycommsize,grid%structured_grid%npx,grid%structured_grid%npy, &
+          grid%structured_grid%npz
+      write(option%fid_out,'(" Actual decomposition: npx,y,z= ",3i4,/)') &
+          grid%structured_grid%npx_final,grid%structured_grid%npy_final, &
+          grid%structured_grid%npz_final
     endif
   endif
 
