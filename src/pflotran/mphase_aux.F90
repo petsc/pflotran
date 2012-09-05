070a59e64ca9c4f15fa3f2b466fd69c96f8d5eee
Satish Karra <unknown>
2012-09-04 21:28:32 -0600

    Modified MphaseSecHeatAuxVarCompute by removing allocatable arrays.

--- src/pflotran/mphase_aux.F90
+++ src/pflotran/mphase_aux.F90
@@ -586,14 +586,21 @@ subroutine MphaseSecHeatAuxVarCompute(sec_heat_vars,aux_var,global_aux_var, &
   type(mphase_auxvar_elem_type) :: aux_var
   type(global_auxvar_type) :: global_aux_var
   type(option_type) :: option
-  PetscReal, allocatable :: coeff_left(:), coeff_diag(:), coeff_right(:)
-  PetscReal, allocatable :: rhs(:), sec_temp(:), area(:), vol(:)
-  PetscReal, allocatable :: dm_plus(:), dm_minus(:)
+  PetscReal :: coeff_left(sec_heat_vars%ncells)
+  PetscReal :: coeff_diag(sec_heat_vars%ncells)
+  PetscReal :: coeff_right(sec_heat_vars%ncells)
+  PetscReal :: rhs(sec_heat_vars%ncells)
+  PetscReal :: sec_temp(sec_heat_vars%ncells)
+  PetscReal :: area(sec_heat_vars%ncells)
+  PetscReal :: vol(sec_heat_vars%ncells)
+  PetscReal :: dm_plus(sec_heat_vars%ncells)
+  PetscReal :: dm_minus(sec_heat_vars%ncells)
   PetscInt :: i, ngcells
   PetscReal :: area_fm
   PetscReal :: alpha, therm_conductivity, dencpr
   PetscReal :: temp_primary_node
   PetscReal :: m
+  
   
   ngcells = sec_heat_vars%ncells
   area = sec_heat_vars%area
@@ -601,14 +608,8 @@ subroutine MphaseSecHeatAuxVarCompute(sec_heat_vars,aux_var,global_aux_var, &
   dm_plus = sec_heat_vars%dm_plus
   dm_minus = sec_heat_vars%dm_minus
   area_fm = sec_heat_vars%interfacial_area
-! temp_primary_node = global_aux_var%temp(1)
   temp_primary_node = aux_var%temp
 
-  allocate(coeff_left(ngcells))
-  allocate(coeff_diag(ngcells))
-  allocate(coeff_right(ngcells))
-  allocate(rhs(ngcells))
-  allocate(sec_temp(ngcells))
   
   coeff_left = 0.d0
   coeff_diag = 0.d0
@@ -660,8 +661,8 @@ subroutine MphaseSecHeatAuxVarCompute(sec_heat_vars,aux_var,global_aux_var, &
 ! print *,'temp_dcdm= ',(sec_temp(i),i=1,ngcells)
   
   sec_heat_vars%sec_temp = sec_temp
-  
-         
+
+
 end subroutine MphaseSecHeatAuxVarCompute
 
 ! ************************************************************************** !
