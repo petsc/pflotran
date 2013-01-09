e7397cbbb2b51aa8e8660ba4e7ffd00d97e1a030
Satish Karra <unknown>
2013-01-09 10:55:57 -0700

    Override logK value for SC CO2 with Henry Duan Sun value before printing.

--- src/pflotran/reaction_aux.F90
+++ src/pflotran/reaction_aux.F90
@@ -221,7 +221,10 @@ module Reaction_Aux_module
     PetscReal, pointer :: eqgash2ostoich(:)  ! stoichiometry of water, if present
     PetscReal, pointer :: eqgas_logK(:)
     PetscReal, pointer :: eqgas_logKcoef(:,:)
-    
+#ifdef CHUAN_CO2        
+    PetscReal :: scco2_eq_logK ! SC CO2 
+#endif    
+
     PetscInt :: nsorb
     PetscInt :: neqsorb
     PetscBool, pointer :: kd_print(:)
@@ -447,6 +450,9 @@ function ReactionCreate()
   nullify(reaction%eqgash2ostoich)
   nullify(reaction%eqgas_logK)
   nullify(reaction%eqgas_logKcoef)
+#ifdef CHUAN_CO2  
+  reaction%scco2_eq_logK = 0.d0
+#endif
   
   reaction%neqcplx = 0
   nullify(reaction%eqcplxspecid)
