9a6f05ec0efbf7509a8565110eec5f51ac0bba75
Glenn Hammond <unknown>
2013-05-16 11:17:00 -0700

    Modified reaction sandbox template; added examples A and B (first-order decay).

--- src/pflotran/reaction_sandbox.F90
+++ src/pflotran/reaction_sandbox.F90
@@ -72,7 +72,7 @@ subroutine RSandboxSetup(reaction,option)
   cur_sandbox => sandbox_list
   do
     if (.not.associated(cur_sandbox)) exit
-    call cur_sandbox%init(reaction,option)
+    call cur_sandbox%Setup(reaction,option)
     cur_sandbox => cur_sandbox%next
   enddo 
 
