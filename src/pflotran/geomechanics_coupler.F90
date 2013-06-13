f4ccf72a1a3e82833b667c39a882a6318b7e5b9b
Satish Karra <satkarra@lanl.gov>
2013-06-12 23:07:58 -0600

    Added create subroutines to Geomechanics_coupler.F90

--- src/pflotran/geomechanics_coupler.F90
+++ src/pflotran/geomechanics_coupler.F90
@@ -1,6 +1,6 @@
 module Geomechanics_Coupler_module
  
-!  use Geomechanics_Condition_module
+  use Geomechanics_Condition_module
   use Geomechanics_Region_module
  
   implicit none
@@ -13,34 +13,146 @@ module Geomechanics_Coupler_module
   ! SK: Note that there is no initial coupler since we solve 
   ! a quasi-static problem for geomechanics (when coupled to flow, otherwise
   ! it is a steady state problem)
-  PetscInt, parameter, public :: GM_BOUNDARY_COUPLER_TYPE = 1
-  PetscInt, parameter, public :: GM_SRC_SINK_COUPLER_TYPE = 2
+  PetscInt, parameter, public                 :: GM_BOUNDARY_COUPLER_TYPE = 1
+  PetscInt, parameter, public                 :: GM_SRC_SINK_COUPLER_TYPE = 2
 
    type, public :: geomech_coupler_type
-    PetscInt :: id                                         ! id of coupler
-    character(len=MAXWORDLENGTH) :: name                   ! name of coupler
-    PetscInt :: itype                                      ! integer defining type
-    character(len=MAXWORDLENGTH) :: ctype                  ! character string defining type
-    character(len=MAXWORDLENGTH) :: geomech_condition_name ! character string defining name of condition to be applied
-    character(len=MAXWORDLENGTH) :: region_name            ! character string defining name of region to be applied
-    PetscInt :: igeomech_condition                         ! id of condition in condition array/list
-    PetscInt :: iregion                                    ! id of region in region array/list
-    PetscInt, pointer :: geomech_aux_int_var(:,:)          ! auxiliary array for integer value
-    PetscReal, pointer :: geomech_aux_real_var(:,:)        ! auxiliary array for real values
- !   type(flow_condition_type), pointer :: flow_condition     ! pointer to condition in condition array/list
-    type(gm_region_type), pointer :: region                ! pointer to region in region array/list
-    type(geomech_coupler_type), pointer :: next                 ! pointer to next coupler
+    PetscInt                                  :: id                           ! id of coupler
+    character(len=MAXWORDLENGTH)              :: name                         ! name of coupler
+    PetscInt                                  :: itype                        ! integer defining type
+    character(len=MAXWORDLENGTH)              :: ctype                        ! character string defining type
+    character(len=MAXWORDLENGTH)              :: geomech_condition_name       ! character string defining name of condition to be applied
+    character(len=MAXWORDLENGTH)              :: region_name                  ! character string defining name of region to be applied
+    PetscInt                                  :: igeomech_condition           ! id of condition in condition array/list
+    PetscInt                                  :: iregion                      ! id of region in region array/list
+    PetscInt, pointer                         :: geomech_aux_int_var(:,:)     ! auxiliary array for integer value
+    PetscReal, pointer                        :: geomech_aux_real_var(:,:)    ! auxiliary array for real values
+    type(geomech_condition_type), pointer     :: geomech_condition            ! pointer to condition in condition array/list
+    type(gm_region_type), pointer             :: region                       ! pointer to region in region array/list
+    type(geomech_coupler_type), pointer       :: next                         ! pointer to next coupler
   end type geomech_coupler_type
   
   type, public :: geomech_coupler_ptr_type
-    type(geomech_coupler_type), pointer :: ptr
+    type(geomech_coupler_type), pointer       :: ptr
   end type geomech_coupler_ptr_type
     
   type, public :: geomech_coupler_list_type
     PetscInt :: num_couplers
-    type(geomech_coupler_type), pointer :: first
-    type(geomech_coupler_type), pointer :: last
-    type(geomech_coupler_type), pointer :: array(:)    
+    type(geomech_coupler_type), pointer       :: first
+    type(geomech_coupler_type), pointer       :: last
+    type(geomech_coupler_type), pointer       :: array(:)    
   end type geomech_coupler_list_type
+
+
+  interface GeomechCouplerCreate
+    module procedure GeomechCouplerCreate1
+    module procedure GeomechCouplerCreate2
+    module procedure GeomechCouplerCreateFromGeomechCoupler
+  end interface
+    
+contains
+
+! ************************************************************************** !
+!
+! GeomechCouplerCreate: Creates a coupler
+! author: Glenn Hammond
+! date: 10/23/07
+!
+! ************************************************************************** !
+function GeomechCouplerCreate1()
+
+  implicit none
+
+  type(geomech_coupler_type), pointer :: GeomechCouplerCreate1
+  
+  type(geomech_coupler_type), pointer :: coupler
+  
+  allocate(coupler)
+  coupler%id = 0
+  coupler%name = ''
+  coupler%itype = GM_BOUNDARY_COUPLER_TYPE
+  coupler%ctype = "boundary"
+  coupler%geomech_condition_name = ""
+  coupler%region_name = ""
+  coupler%igeomech_condition = 0
+  coupler%iregion = 0
+  nullify(coupler%geomech_aux_int_var)
+  nullify(coupler%geomech_aux_real_var)
+  nullify(coupler%geomech_condition)
+  nullify(coupler%region)
+  nullify(coupler%next)
+  
+  GeomechCouplerCreate1 => coupler
+
+end function GeomechCouplerCreate1
+
+! ************************************************************************** !
+!
+! GeomechCouplerCreate2: Creates a coupler
+! author: Glenn Hammond
+! date: 10/23/07
+!
+! ************************************************************************** !
+function GeomechCouplerCreate2(itype)
+
+  implicit none
+
+  PetscInt :: itype
+  
+  type(geomech_coupler_type), pointer :: GeomechCouplerCreate2
+  
+  type(geomech_coupler_type), pointer :: coupler
+  
+  coupler => GeomechCouplerCreate1()
+  coupler%itype = itype
+  select case(itype)
+    case(GM_BOUNDARY_COUPLER_TYPE)
+      coupler%ctype = 'boundary'
+    case(GM_SRC_SINK_COUPLER_TYPE)
+      coupler%ctype = 'source_sink'
+  end select
+
+  GeomechCouplerCreate2 => coupler
+
+end function GeomechCouplerCreate2
+
+! ************************************************************************** !
+!
+! GeomechCouplerCreateFromGeomechCoupler: Creates a coupler
+! author: Glenn Hammond
+! date: 10/23/07
+!
+! ************************************************************************** !
+function GeomechCouplerCreateFromGeomechCoupler(coupler)
+
+  implicit none
+  
+  type(geomech_coupler_type), pointer :: coupler
+  
+  type(geomech_coupler_type), pointer :: GeomechCouplerCreateFromGeomechCoupler
+  type(geomech_coupler_type), pointer :: new_coupler
+
+  new_coupler => GeomechCouplerCreate1()
+
+  new_coupler%id = coupler%id
+  new_coupler%name = coupler%name
+  new_coupler%itype = coupler%itype
+  new_coupler%ctype = coupler%ctype
+  new_coupler%geomech_condition_name = coupler%geomech_condition_name
+  new_coupler%region_name = coupler%region_name
+  new_coupler%igeomech_condition = coupler%igeomech_condition
+  new_coupler%iregion = coupler%iregion
+
+  ! these must remain null  
+  nullify(coupler%geomech_condition)
+  nullify(coupler%region)
+  nullify(coupler%geomech_aux_int_var)
+  nullify(coupler%geomech_aux_real_var)
+  nullify(coupler%next)
+
+  GeomechCouplerCreateFromGeomechCoupler => new_coupler
+
+end function GeomechCouplerCreateFromGeomechCoupler
+
   
 end module Geomechanics_Coupler_module
