340c802e6308b545f944d71435d13e1eef0719e5
Glenn Hammond <unknown>
2013-12-23 13:02:32 -0800

    Updated th/thc regression gold file to account for derivative of psat with respect to temperature in the water viscosity derivative calc.

--- src/pflotran/eos_water.F90
+++ src/pflotran/eos_water.F90
@@ -171,6 +171,7 @@ subroutine EOSWaterViscosity1(T, P, PS, dPS_dt, VW, calculate_derivatives, &
             ! dpwr_EX_dt
             VW*aln10*247.8d0/(t+133.15d0)**2
     dVWdp = VW/AM*PHI*1.d-11
+    dVWdps = -1.d0 * dVWdp
   else
     dVWdt = -999.d0
     dVWdp = -999.d0
