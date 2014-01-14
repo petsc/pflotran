e1af338ee67692054d78efc6851d724dd81b2bcb
Glenn Hammond <unknown>
2014-01-14 09:10:27 -0800

    Commented out the destruction of unstructured DM in commX

--- src/pflotran/unstructured_communicator.F90
+++ src/pflotran/unstructured_communicator.F90
@@ -248,7 +248,9 @@ subroutine UnstructuredCommunicatorDestroy(this)
   endif
   nullify(this%ugdm)
   if (this%dm /= 0) then
-    call DMDestroy(this%dm,ierr)
+    !geh: all DMs are currently destroyed in realization.  This DM is solely
+    !     a pointer.  This will need to change, but skip for now.
+    !call DMDestroy(this%dm,ierr)
   endif
   this%dm = 0  
   
