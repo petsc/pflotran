62099b90e6d1c02bfcde010e3811d39f6b375a7e
Glenn Hammond <unknown>
2014-02-27 11:25:11 -0800

    Removed duplicated destructiono of simulation_old

--- src/pflotran/subsurface_factory.F90
+++ src/pflotran/subsurface_factory.F90
@@ -78,11 +78,6 @@ subroutine SubsurfaceInitializePostPetsc(simulation, option)
   simulation_old => SimulationCreate(option)
   call Init(simulation_old)
   call HijackSimulation(simulation_old,simulation)
-  ! no longer need simulation
-  ! nullify realization and regression so that it is not destroyed
-  nullify(simulation_old%realization)
-  nullify(simulation_old%regression)
-  call SimulationDestroy(simulation_old)
   call SubsurfaceJumpStart(simulation)
   
   ! no longer need simulation
