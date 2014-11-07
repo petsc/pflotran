51a6ef30757416a8626088362985158339a45201
Glenn Hammond <unknown>
2014-11-07 15:48:33 -0800

    Cleaning up code and makefile

--- src/pflotran/patch.F90
+++ src/pflotran/patch.F90
@@ -2600,15 +2600,10 @@ subroutine PatchInitCouplerConstraints(coupler_list,reaction,option)
           global_auxvar%temp = option%reference_temperature
         endif
 
-#ifndef DONT_USE_WATEOS
         call EOSWaterDensity(global_auxvar%temp, &
                              global_auxvar%pres(1), &
                              global_auxvar%den_kg(1), &
                              dum1,ierr)
-#else
-        call EOSWaterdensity(global_auxvar%temp,global_auxvar%pres(1), &
-                             global_auxvar%den_kg(1),dum1,ierr)
-#endif                     
       else
         global_auxvar%pres = option%reference_pressure
         global_auxvar%temp = option%reference_temperature
