43eabd988b32ca8d0c405c8b6b8daa1502a55d4e
Glenn Hammond <unknown>
2015-02-24 16:25:51 -0800

    Fixed TOUGH2 convergence criteria

--- src/pflotran/general.F90
+++ src/pflotran/general.F90
@@ -3534,6 +3534,7 @@ subroutine GeneralCheckUpdatePost(line_search,X0,dX,X1,dX_changed, &
   PetscBool :: converged_rel_update
   PetscBool :: converged_scaled_residual
   PetscInt :: istate
+  PetscReal :: t_over_v
   PetscErrorCode :: ierr
   
   grid => realization%patch%grid
@@ -3580,9 +3581,16 @@ subroutine GeneralCheckUpdatePost(line_search,X0,dX,X1,dX_changed, &
         inf_norm_residual(idof,istate) = max(inf_norm_residual(idof,istate), &
                                              dabs(R))
         if (general_tough2_conv_criteria) then
+#ifdef WRONG
           if (accum_p2(ival) < general_tough2_itol_scaled_res_e2) then
             R_A = dabs(R/general_tough2_itol_scaled_res_e2)
-!geh            R_A = dabs(R/material_auxvars(ghosted_id)%volume*option%flow_dt)
+#else
+          !geh: scale by t_over_v to match TOUGH2 residual units. see equation
+          !     B.5 of TOUGH2 user manual (LBNL-43134)
+          t_over_v = option%flow_dt/material_auxvars(ghosted_id)%volume
+          if (accum_p2(ival)*t_over_v < general_tough2_itol_scaled_res_e2) then
+            R_A = dabs(R*t_over_v)
+#endif
           else
             R_A = dabs(R/accum_p2(ival))
           endif
