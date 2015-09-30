15a76702756f25298d0eb12d410531888a19a144
Glenn Hammond <unknown>
2015-09-29 18:16:19 -0700

    Updated python script for creating regression test.

--- src/python/gridded_dataset_loader.py
+++ src/python/gridded_dataset_loader.py
@@ -22,7 +22,7 @@ h5grp.attrs['Max Buffer Size'] = [4]
 rarray = numpy.zeros((nt),'=f8')
 for t in range(nt):
   rarray[t] = 10.*float(t)
-h5dset = h5grp.create_dataset('time', data=rarray)
+h5dset = h5grp.create_dataset('Times', data=rarray)
 
 rarray = numpy.zeros((nx,ny,nt),'=f8')
 
@@ -31,7 +31,7 @@ for t in range(nt):
     for i in range(nx):
       rarray[i][j][t] = float(i) + 1.e-2*float(j) + 2. + \
                         1.e2*float(t)
-h5dset = h5grp.create_dataset('data', data=rarray)
+h5dset = h5grp.create_dataset('Data', data=rarray)
 
 # 1d line
 h5grp = h5file.create_group('x_line')
@@ -47,7 +47,7 @@ rarray = numpy.zeros(nx,'=f8')
 
 for i in range(nx):
   rarray[i] = 5. - 0.1*float(i)
-h5dset = h5grp.create_dataset('data', data=rarray)
+h5dset = h5grp.create_dataset('Data', data=rarray)
  
 # 1d line
 h5grp = h5file.create_group('y_line')
@@ -66,14 +66,14 @@ h5grp.attrs['Max Buffer Size'] = [2]
 rarray = numpy.zeros((nt),'=f8')
 for t in range(nt):
   rarray[t] = 5.*float(t)
-h5dset = h5grp.create_dataset('time', data=rarray)
+h5dset = h5grp.create_dataset('Times', data=rarray)
 
 rarray = numpy.zeros((ny,nt),'=f8')
 
 for t in range(nt):
   for j in range(ny):
     rarray[j][t] = 5. + 0.1*float(j) + float(t)
-h5dset = h5grp.create_dataset('data', data=rarray)
+h5dset = h5grp.create_dataset('Data', data=rarray)
  
 h5file.close()
 
