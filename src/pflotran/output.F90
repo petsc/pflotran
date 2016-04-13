c5004abc66ef051bdc062c12c17c51269e398b1e
Jenn Frederick <jmfrede@sandia.gov>
2016-04-13 13:39:41 -0600

    Fixed mistake in mass_balance_file DETAILED.

--- src/pflotran/output.F90
+++ src/pflotran/output.F90
@@ -443,19 +443,10 @@ subroutine OutputFileRead(realization,output_option,waypoint_list,block_name)
         
 !.............................
       case('DETAILED')
-        call InputReadWord(input,option,word,PETSC_TRUE)
-        call InputDefaultMsg(input,option, &
-                           'OUTPUT,MASS_BALANCE,DETAILED')
-        if (len_trim(word) > 0) then
-          call StringToUpper(word)
-          select case(trim(word))
-            case('DETAILED')
-              option%mass_bal_detailed = PETSC_TRUE
-            case default
-              call InputKeywordUnrecognized(word, &
-                                            'OUTPUT,MASS_BALANCE',option)
-          end select
-        endif
+        select case(trim(block_name))
+          case('MASS_BALANCE_FILE') 
+            option%mass_bal_detailed = PETSC_TRUE
+        end select
 
 !...............................
       case('VELOCITY_AT_CENTER')
