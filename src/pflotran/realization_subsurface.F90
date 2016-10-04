97f1b0b6f24755f845df789e85872fbab11a165d
Glenn Hammond <unknown>
2016-10-04 13:04:15 -0700

    At Nate's request, destroying realization's region list immediately after regions are distributed to patches to avoid confusion regardin which region list is used.

--- src/pflotran/realization_subsurface.F90
+++ src/pflotran/realization_subsurface.F90
@@ -494,15 +494,19 @@ subroutine RealizationLocalizeRegions(realization)
 
   call PatchLocalizeRegions(realization%patch,realization%region_list, &
                             realization%option)
+  ! destroy realization's copy of region list as it can be confused with the
+  ! localized patch regions later in teh simulation.
+  call RegionDestroyList(realization%region_list)
 
   ! compute regional connections for inline surface flow
   if (option%inline_surface_flow) then
      region => RegionGetPtrFromList(option%inline_surface_region_name, &
           realization%patch%region_list)
      if (.not.associated(region)) then
-        option%io_buffer = "realization_subsurface.F90:RealizationLocalizeRegions() --> " // &
-             "Could not find a required region named '" // &
-             trim(option%inline_surface_region_name) // "' from the list of regions."
+        option%io_buffer = 'realization_subsurface.F90:RealizationLocalize&
+             &Regions() --> Could not find a required region named "' // &
+             trim(option%inline_surface_region_name) // &
+             '" from the list of regions.'
         call printErrMsg(option)
      endif
      call GridRestrictRegionalConnect(realization%patch%grid,region)
