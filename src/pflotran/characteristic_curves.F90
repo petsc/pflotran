8cb65c193068430e1cefff8cb2919019ba965ed4
Glenn Hammond <unknown>
2017-02-02 12:47:42 -0800

    Fixed module name utility_module -> Utility_module

--- src/pflotran/characteristic_curves.F90
+++ src/pflotran/characteristic_curves.F90
@@ -4162,7 +4162,7 @@ subroutine SF_mK_CapillaryPressure(this,liquid_saturation, &
   ! Date: 2017
   !
   use Option_module
-  use utility_module, only : InverseNorm
+  use Utility_module, only : InverseNorm
 
   implicit none
 
@@ -6820,7 +6820,7 @@ subroutine RPF_mK_Gas_RelPerm(this,liquid_saturation, &
   ! Date: 2017
   !
   use Option_module
-  use utility_module, only : InverseNorm
+  use Utility_module, only : InverseNorm
 
   implicit none
 
