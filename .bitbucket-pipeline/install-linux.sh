4dc4204bdfed36adb7c5ace8aaf19487bb1c63d9
Glenn Hammond <unknown>
2017-03-21 09:57:31 -0700

    Altered pipline scripts for compilation against petsc master and the addition of zlib for hdf5 compression.

--- .bitbucket-pipeline/install-linux.sh
+++ .bitbucket-pipeline/install-linux.sh
@@ -5,18 +5,19 @@ sudo apt-get install -y cmake gcc libopenmpi-dev openmpi-bin liblapack-dev gfort
 
 git clone https://bitbucket.org/petsc/petsc.git
 
-PETSC_GIT_HASH=`cat tools/buildbot/petsc/petsc-git-version.txt`
+#PETSC_GIT_HASH=`cat tools/buildbot/petsc/petsc-git-version.txt`
 
 cd petsc
 
-git checkout ${PETSC_GIT_HASH}
+# pflotran-xsdk follows the tip (master branch) of PETSc
+#git checkout ${PETSC_GIT_HASH}
 
 export PETSC_DIR=$PWD
 export PETSC_ARCH=linux-gnu
 export PETSC_WITH_HDF5=1
 
 #if [ ${PETSC_WITH_HDF5} == 1 ]; then
-./configure PETSC_ARCH=linux-gnu --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1 --download-hdf5 --download-metis --download-parmetis
+./configure PETSC_ARCH=linux-gnu --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1 --download-hdf5 --download-metis --download-parmetis --download-zlib
 #elif [ ${PETSC_WITH_HDF5} == 0 ]; then
 #  ./configure PETSC_ARCH=linux-gnu --with-mpi=1 --with-debug=$DEBUG --with-shared-libraries=1 --download-metis --download-parmetis
 #else
